acde8bb470d58cae46e286b7bd82201b6ab821ae
DrWhilson <DrWhilsonMFI@gamil.com>
2023-10-28 21:47:56 +0700

    OwO

--- AScript.sh
+++ AScript.sh
@@ -1,7 +1,4 @@
 echo "Ok now fonts"
-#sudo cp ./3270.zip /usr/local/share/fonts
-#sudo unzip /usr/local/share/fonts/3270.zip
-#sudo rm /usr/local/share/fonts/3270.zip
 mkdir buff
 mv ./3270.zip ./buff/3270.zip
 cd ./buff
--- DList.sh
+++ DList.sh
@@ -5,6 +5,7 @@ echo "This is you term"
 sudo apt install cool-retro-term
 sudo snap install nvim --classic
 echo "some progers stuff"
+sudo apt install python-is-python3
 sudo apt install git
 sudo apt install gcc
 sudo apt install cpputest
