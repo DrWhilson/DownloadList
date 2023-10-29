3b5ef458cac1ebae62581ddfd2b5605672b8329c
DrWhilson <DrWhilsonMFI@gmail.com>
2023-10-29 22:09:22 +0700

    ^w^

--- DList.sh
+++ DList.sh
@@ -1,5 +1,6 @@
 echo "I try this s#*@"
 sudo apt update
+sudo apt upgrade
 sudo apt install snap
 echo "This is you term"
 sudo apt install cool-retro-term
@@ -7,8 +8,20 @@ sudo snap install nvim --classic
 echo "some progers stuff"
 sudo apt install python3
 sudo apt install python3-venv
-sudo apt install clang
-sudo apt install gcc
+
+#gcc
+sudo apt-get install build-essential
+#clang
+sudo apt-get install clang
+sudo snap install clangd --classic
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+sudo ./llvm.sh 16
+rm llvm.sh
+sudo update-alternatives --install /usr/bin/clang++ clang++ /usr/bin/clang++-16 100
+sudo update-alternatives --install /usr/bin/clang clang /usr/bin/clang-16 100
+sudo update-alternatives --config cc
+
 sudo apt install cpputest
 sudo apt install valgrind
 sudo apt install cmake
