a74755dd3a0f8a5fd708079036d0a7dcc63c77f7
DrWhilson <DrWhilsonMFI@gmail.com>
2023-11-17 10:33:39 +0700

    >_<

--- DList.sh
+++ DList.sh
@@ -1,4 +1,4 @@
-echo "I try this s#*@"
+echo "I try this ;~;"
 sudo apt update
 sudo apt upgrade
 sudo apt install snap
@@ -11,12 +11,23 @@ sudo apt install python3-venv
 
 #gcc
 sudo apt-get install build-essential
-sudo apt install libstdc++-12-dev 
-sudo update-alternatives --config cc
 
-sudo apt install cpputest
 sudo apt install valgrind
 sudo apt install cmake
+# sudo apt install libstdc++-12-dev
+# sudo update-alternatives --config cc
+
+# echo "Install CppUTest"
+# sudo apt install cpputest
+
+# echo "Install GTest"
+# sudo apt-get install libgtest-dev
+# cd /usr/src/gtest
+# sudo cmake CMakeLists.txt
+# sudo make
+# cd ./lib
+# sudo cp *.a /usr/lib
+
 echo "some programs"
 sudo apt install brasero
 sudo apt install gimp
