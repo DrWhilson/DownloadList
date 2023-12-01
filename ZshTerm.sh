d3a7a0537e155a5c98e67e7d76632486885af184
DrWhilson <DrWhilsonMFI@gmail.com>
2023-12-01 15:05:52 +0700

    I_I

--- ZshTerm.sh
+++ ZshTerm.sh
@@ -2,7 +2,13 @@ echo "Ok, cool terminal"
 sudo apt install zsh
 sudo apt install curl
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp ./.zshrc ~/.zshrc
+
+sed '/plugins/d' ~/.zshrc
+echo 'plugins=(git zsh-syntax-highlighting)' >>~/.zshrc
+
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
-cp ./.p10k.zsh ~/.p10k.zsh
+
+echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
+echo 'POWERLEVEL9K_MODE="nerdfont-complete"' >> ~/.zshrc
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
