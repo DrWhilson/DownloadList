707ab1f14e525271e0a20be8198124ab04647e65
DrWhilsom <DrWhilsonMFI@gmail.com>
2023-12-15 23:05:52 +0700

    Aaaarch

--- ZshTerm.sh
+++ ZshTerm.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 echo "Ok, cool terminal"
-sudo apt install zsh
-sudo apt install curl
+if [[ "$1" == "Ubuntu" ]]; then 
+  sudo apt install zsh
+  sudo apt install curl
+elif [[ "$1" == "Arch" ]]; then
+  sudo pacman -S zsh
+  sudo pacman -S curl
+else
+  echo "Set system as param: Ubuntu or Arch" 
+fi
+
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 sed '/plugins/d' ~/.zshrc
 echo 'plugins=(git zsh-syntax-highlighting)' >>~/.zshrc
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
 
 echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >> ~/.zshrc
 echo 'POWERLEVEL9K_MODE="nerdfont-complete"' >> ~/.zshrc
