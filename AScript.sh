215a16566cd36fd2efba83e911f540a8e008055e
DrWhilson <drwhilsonmfi@gmail.com>
2025-03-05 02:31:40 +0700

    fix config file setup

--- AScript.sh
+++ AScript.sh
@@ -11,4 +11,7 @@ sudo rm -r ~/.config/nvim
 git clone --depth 1 https://github.com/AstroNvim/template ~/.config/nvim
 rm -rf ~/.config/nvim/.git
 
+sed -i 's/if true/-- if true/g' ~/.config/nvim/lua/plugins/astrocore.lua
+sed -i 's/relativenumber = true/relativenumber = false/g' ~/.config/nvim/lua/plugins/astrocore.lua
+
 echo "Done!"
