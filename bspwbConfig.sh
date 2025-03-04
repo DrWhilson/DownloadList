f06dfb875d59f23dfab91f5057eb103777b2393e
DrWhilson <drwhilsonmfi@gmail.com>
2025-03-04 19:26:48 +0700

    [ ._.]

--- bspwbConfig.sh
+++ bspwbConfig.sh
@@ -1,1 +1,7 @@
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+export PYENV_VIRTUALENV_DISABLE_PROMPT=1
 
+eval "$(pyenv init --path)"
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
