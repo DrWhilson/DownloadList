1da9caef27d528974d5ca0b700fe83ec52c38c0c
DrWhilson <drwhilsonmfi@gmail.com>
2025-03-05 02:38:04 +0700

    delete fix

--- pyenvfix.sh
+++
@@ -1,7 +0,0 @@
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-export PYENV_VIRTUALENV_DISABLE_PROMPT=1
-
-eval "$(pyenv init --path)"
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
